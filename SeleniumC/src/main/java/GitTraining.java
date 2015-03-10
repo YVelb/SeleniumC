801e3e153691eadda274b92427cbf0db6715e22e
YVelb <logoriniel@ukr.net>
2015-03-10 20:09:21 +0200

    Added new project

---
+++ SeleniumC/src/main/java/GitTraining.java
@@ -0,0 +1,17 @@
+/**
+ * Created by Yana on 3/10/2015.
+ */
+public class GitTraining {
+
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 6;
+        int x;
+        x = a + b;
+
+        System.out.println("a: " + a);
+        System.out.println("b: " + b);
+        System.out.println("b + a = " + x);
+
+    }
+}
